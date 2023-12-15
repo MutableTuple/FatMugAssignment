d4fd291f63b1d05c71ec36cfc4b0287330e973ba
Yogesh Vishwakarma <96367458+MutableTuple@users.noreply.github.com>
2023-12-15 09:28:32 +0530

    Delete Fatmug directory

--- Fatmug/vendor/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import vendorModel,PurchaseOrder,PerformanceRecord
-# Register your models here.
-
-admin.site.register(vendorModel)
-admin.site.register(PurchaseOrder)
-admin.site.register(PerformanceRecord)
